40395ae58f2ab0ab9460d78f235185fe19414604
Pradeep Sanchana <pradeep_sanchana@marketo.com>
2018-06-20 19:48:22 +0530

    lcm and String...

---
+++ src/lcm.scala
@@ -0,0 +1,57 @@
+import scala.collection.mutable.HashMap
+
+/**
+ * Created by pradeepsanchana on 20/06/18.
+ */
+object lcm {
+  def main(args: Array[String]) {
+    println(getLcm(20,30))
+    println(getLcm(40,30))
+    println(getLcm(getLcm(20,30),40))
+  }
+
+  def getLcm(x:Int, y:Int):Int= {
+    var l = findAllMul(x)
+    var m = findAllMul(y)
+    var p=0
+    var q=0
+    var sum = 1.0
+    var finalV = HashMap[Int,Int]()
+    for(i <- l.keysIterator ){
+      p = l(i)
+      q = m(i)
+      m.remove(i)
+      finalV += (i -> Math.max(p,q))
+    }
+    finalV ++= m
+    for(i<- finalV.keysIterator){
+      p = finalV(i)
+      sum *= Math.pow(i.toDouble,p.toDouble)
+    }
+    sum.toInt
+  }
+
+  def findAllMul(x:Int) ={
+    var i = x
+    var j = 2
+    var ret = HashMap[Int,Int]()
+    var num = 1
+    while ( j <= i){
+      if(i%j == 0){
+        if( ret.contains(j)){
+          var num = ret(j)
+          num += 1
+          ret(j) = num
+        } else{
+          ret += (j -> num)
+        }
+        i /= j
+      } else {
+        j += 1
+      }
+    }
+    ret
+
+  }
+
+}
---
+++ src/test.java
@@ -0,0 +1,16 @@
+/**
+ * Created by pradeepsanchana on 19/06/18.
+ */
+public class test {
+    public static void app(String... aa){
+        System.out.println(aa.length);
+        System.out.println(aa);
+    }
+    public static void main(String[] args){
+
+        app("i ","am ","good.");
+
+        app();
+    }
+
+}
